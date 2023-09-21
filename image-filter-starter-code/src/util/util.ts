c1a56aa07fe583fc0c9f0d34d0f090b6c7934c1a
ShinCN <hungnq9120@gmail.com>
2023-09-21 16:54:00 +0700

    fix bug eb init

--- image-filter-starter-code/src/util/util.ts
+++ image-filter-starter-code/src/util/util.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import fs from "fs";
 import Jimp = require("jimp");
 
@@ -11,15 +12,16 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const photoBuffer = await axios.get(inputURL, { responseType: "arraybuffer", });
+      const photo = await Jimp.read(Buffer.from(photoBuffer?.data, "binary"));
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
-          resolve(__dirname + outpath);
+        .write(outpath, (img) => {
+          resolve(outpath);
         });
     } catch (error) {
       reject(error);
@@ -37,3 +39,7 @@ export async function deleteLocalFiles(files: Array<string>) {
     fs.unlinkSync(file);
   }
 }
+ 
+
+ 
+
